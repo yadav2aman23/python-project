210dde3af9e57a2eeb3902fba50136a5dbc4af05
Yadav <yadav@Yadav.Yadav>
2026-08-31 20:08:11 +0530

    Auto update: 2026-08-31 20:08:11

--- allcode/test.py
+++ allcode/test.py
@@ -1,6 +1,9 @@
-#include<iostream>
-#include<string>
-# using namespace std;
-
-
-
+age=int(input("Enter your age:"))
+if age>=18:
+    print("yes you are eligiable to vote")
+elif age>=13:
+    print("You have wating more yers to vote:")
+else:
+    print("You are now bacha so you could not to vote ,ghar jawo bache ")
+    
+        
