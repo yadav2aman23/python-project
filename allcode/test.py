e4bdde34275c530beafbe38839549a19bb43ba68
Yadav <yadav@Yadav.Yadav>
2026-08-31 20:13:15 +0530

    Auto update: 2026-08-31 20:13:15

--- allcode/test.py
+++ allcode/test.py
@@ -1,9 +1,22 @@
-age=int(input("Enter your age:"))
+'''age=int(input("Enter your age:"))
 if age>=18:
     print("yes you are eligiable to vote")
 elif age>=13:
     print("You have wating more yers to vote:")
 else:
     print("You are now bacha so you could not to vote ,ghar jawo bache ")
-    
-        
+
+        '''
+
+
+marks=int(input("Enter your marks: "))
+if marks>=90:
+    print("You are pass all subject and good job beta")
+elif marks>=80:
+    print("You are pass all subject and do better next time")
+elif marks>=70:
+    print("you are pass all subject and do better next exam aman")
+elif marks<=60:
+    print("You are fail in all subject and do more hard work next time")
+else:
+    print("fill")
