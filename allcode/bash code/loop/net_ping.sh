6aa2edb70e397af004266c77adb5a0138bc3e1f0
Yadav <yadav@Yadav.Yadav>
2026-09-02 09:43:25 +0530

    Auto update: 2026-09-02 09:43:25

--- allcode/bash code/loop/net_ping.sh
+++ allcode/bash code/loop/net_ping.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-for ip in google.com facebook.com 
-do
-    echo "Checking $ip"
-    if ping -c 1 -w 1 "$ip" > /dev/null 2>&1
+for ip in google.com facebook.com
+do 
+    echo "Cheking the ip $ip"
+    if  ping -c 1 -w1 "$ip" > /dev/null 2>&1
+        echo "$ip is up "
     then
-        echo "$ip is up"
     else
         echo "$ip is down"
-
     fi
 done
