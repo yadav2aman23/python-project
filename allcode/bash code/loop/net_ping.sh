1b29b3d3fb9c26d9e33ac70842c6939c8d7b988a
Yadav <yadav@Yadav.Yadav>
2026-09-02 09:35:58 +0530

    Auto update: 2026-09-02 09:35:58

---
+++ allcode/bash code/loop/net_ping.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for ip in google.com facebook.com 
+do
+    echo "Checking $ip"
+    if ping -c 1 -w 1 "$ip" > /dev/null 2 >&1
+    then
+        echo "$ip is up"
+    else
+        echo "$ip is down"
+
+    fi
