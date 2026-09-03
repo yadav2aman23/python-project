e5a5d8ef96b4ba9c518d349861e0e29aba731af5
Yadav <yadav@Yadav.Yadav>
2026-09-03 10:24:41 +0530

    Auto update: 2026-09-03 10:24:41

---
+++ allcode/opps_code/text.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class BankAccount
+{
+    string name;
+    int AccountNumber;
+    double balance;
+
+    BankAccount(string n, int acc, double bal)
+    {
+        name = n;
+        AccountNumber = acc;
+        balance = bal;
+    }
+};
+void display()
+{
+    cout << "Name" << name << endl;
+    cout << "Account NUmber " << AccountNumber << endl;
+    cout << "Balance" << balance << endl;
+}
+int main()
+{
+    BankAccount account1("aman", 123456, 80000);
+    account1.display();
+    return 0;
+}
