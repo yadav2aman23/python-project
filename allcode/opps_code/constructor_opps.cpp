2cff89c2e37ab6b9f2c200712e83113853582172
Yadav <yadav@Yadav.Yadav>
2026-09-03 08:19:21 +0530

    Auto update: 2026-09-03 08:19:21

---
+++ allcode/opps_code/constructor_opps.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class BankAccount
+{
+public:
+    string name;
+    int AccountNumber;
+    double balance;
+
+    // constructor
+
+    BankAccount(string n; int acc; double bal;)
+    {
+        name = n;
+        AccountNumber = acc;
+        balance = bal;
+    }
+    void display()
+    {
+        cout << "name" << name << endl;
+        cout << "Account number " << AccountNumber << endl;
+        cout << "balance" << balance << endl;
+    }
+};
+
+int main()
+{
+    BankAccount account1("Aman", 1123454, 8000)
+        account1.display();
+    return 0;
+}
