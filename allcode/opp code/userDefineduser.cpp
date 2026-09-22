7296356038a7395106bfb409275e363b71951f5e
Yadav <yadav@Yadav.Yadav>
2026-09-22 18:52:07 +0530

    Auto update: 2026-09-22 18:52:07

---
+++ allcode/opp code/userDefineduser.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class student
+{
+    string name;
+    float cgpa;
+    int rollno;
+    double marks;
+} int main()
+{
+    student s1;
+    cin >> s1.name >> s1.cgpa >> s1.rollno >> s1.marks;
+    cout << "Enter the name " s1.name << "Enter the cgpa" s1.cgpa << "Enter the rollno" s1.rollno << "Entee the marks" s1.marks;
+}
