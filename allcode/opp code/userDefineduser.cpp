16649e80bd7aed4c6e12ac5cfe86fb4e08fc09e9
Yadav <yadav@Yadav.Yadav>
2026-09-22 19:21:21 +0530

    Auto update: 2026-09-22 19:21:21

--- allcode/opp code/userDefineduser.cpp
+++ allcode/opp code/userDefineduser.cpp
@@ -14,5 +14,7 @@ int main()
     student s1;
 
     cin >> s1.name >> s1.cgpa >> s1.rollno >> s1.marks;
+
     cout << "Enter the name " << s1.name << "Enter the cgpa" << s1.cgpa << "Enter the rollno" << s1.rollno << "Entee the marks" << s1.marks;
+    return 0;
 };
