ab698e4af70c356598db070457872458600f890d
Yadav <yadav@Yadav.Yadav>
2026-09-23 09:23:57 +0530

    Auto update: 2026-09-23 09:23:57

---
+++ allcode/opp code/inline_function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int product(int a, int b)
+{
+    a = 5;
+    b = 2;
+}
+int main()
+{
+    cout << "Enter the numer a ";
+    cin >> a;
+    cout << "Enter the number b ";
+    cin >> b;
+
+    return 0;
+}
