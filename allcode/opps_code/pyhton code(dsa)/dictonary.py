00ea1f25be144728769c3936668073b1bc4896f3
Yadav <yadav@Yadav.Yadav>
2026-09-24 19:49:49 +0530

    Auto update: 2026-09-24 19:49:49

--- allcode/opps_code/pyhton code(dsa)/dictonary.py
+++ allcode/opps_code/pyhton code(dsa)/dictonary.py
@@ -33,7 +33,7 @@ for items in student.items():
 
 
 students={
-    student1={"name":"aman","age":25,"rollno":485}
-    student2={"name":"kumar","age":54,"rollno":4756}
+    student1:{"name":"aman","age":25,"rollno":485}
+    student2:{"name":"kumar","age":54,"rollno":4756}
 }
 print(students)
