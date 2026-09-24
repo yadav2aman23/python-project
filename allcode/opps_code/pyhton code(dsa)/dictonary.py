58c0a3f30a7f167c6a690c0067e3755d99ff3d24
Yadav <yadav@Yadav.Yadav>
2026-09-24 19:37:35 +0530

    Auto update: 2026-09-24 19:37:35

--- allcode/opps_code/pyhton code(dsa)/dictonary.py
+++ allcode/opps_code/pyhton code(dsa)/dictonary.py
@@ -3,5 +3,8 @@ student={"name":"Aman","age":45,"Grade":"A"}
 ##print(student["name"])
 ##print(student["age"])
 
-print(student.get("name"))
-print(student.get("name_null","kumar"))
+##print(student.get("name"))
+##print(student.get("name_null","kumar"))
+
+
+student=["name"]="aman"
